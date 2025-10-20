3a4898e9a831cb6ad362ddd7d22bb1017d608c9b
Vadim Silagin <silagin0205@gmail.com>
2025-10-20 18:25:03 +0500

    добавление gitignore

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
 const token = process.env.BOT_TOKEN;
-const bot = new Bot (token, {polling: true})
+const bot = new TelegramBot (token, {polling: true})
 
 bot.onText(/\/start/, (msg) => {
-    const chadId = msg.chat.id;
+    const chatId = msg.chat.id;
     bot.sendMessage(chatId, 'Вставь сюда ссылку на видео')
 })
